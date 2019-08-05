f41f2f44c919e12cb1bfaab33dae74c28c2950ba
cuili <549560976@qq.com>
2019-08-05 16:34:40 +0800

    dist

---
+++ vue/test/dist/precache-manifest.5578d9f3b9b2a81675f3b7cc24218d95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "32632b299bb23a9aab65",
+    "url": "/js/chunk-vendors.275ae397.js"
+  },
+  {
+    "revision": "e2ed4ebcf7f1b8e05c43",
+    "url": "/js/app.ff9fc3f1.js"
+  },
+  {
+    "revision": "9fbc6b23457c15d4bf2e",
+    "url": "/js/about.fd1846b8.js"
+  },
+  {
+    "revision": "65edb47efc165a5a0211a6c3994e68a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e2ed4ebcf7f1b8e05c43",
+    "url": "/css/app.777f455a.css"
+  }
+];
